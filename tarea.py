63ecae6464bbb85d6a9f3084c0c43b2dd687f248
Jose Araya <joseguillermo.araya@gmail.com>
2015-10-23 01:27:36 -0300

    crear y graficar caja

--- tarea.py
+++ tarea.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-ANCHO = 10
-ALTO = 15
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator, FixedLocator, FormatStrFormatter
+import pdb
+
+ANCHO = 3.+1
+ALTO = 4.+1
+H = 1
 
 def crear_caja(ancho , alto, h):
     '''recibe las dimensiones de la caja  y el tamaño del reticulado
      y la inicia en puros ceros '''
+    ancho_efectivo = ancho/h
+    alto_efectivo = alto/h
+    caja = np.zeros ((alto_efectivo,ancho_efectivo) )
+
+    return caja
 
 
 def poner_condiciones_borde(caja , condiciones):
@@ -31,11 +43,35 @@ def converge():
     '''compara 2 estados de la malla, y decide si converge según la tolerancia
     pedida'''
 
-def mostrar():
-    '''imprime el arreglo en consola'''
 
+def f(x,y):
+    '''función auxilar para plotear, recibe la coordenada x e y y devuelve
+    el valor de la malla en esas coordenadas'''
+    return caja[y,x]
+
+
+def mostrar(caja):
+    '''plotea la solución en 3D'''
+    #pdb.set_trace()
+    (alto,ancho) = caja.shape
+    x = np.linspace(0,ancho-1,ancho)
+    y = np.linspace(0,alto-1,alto)
+
+    xg, yg = np.meshgrid(x, y)
+
+    vector_f = np.vectorize(f)
+    z = vector_f(xg,yg)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    #ax.plot_surface(xg, yg, z, rstride=1, cstride=1, cmap=cm.coolwarm,
+    #    linewidth=0, antialiased=False)
+    surf = ax.plot_surface(xg, yg, z, rstride=1, cstride=1,
+                           linewidth=0, antialiased=False, shade=False)
+    plt.show()
 #main
-caja = crear_caja()
+#pdb.set_trace()
+caja = crear_caja(ANCHO,ALTO,H)
 '''
 caja = poner_condiciones_borde()
 caja = poner_linea()
@@ -48,4 +84,4 @@ while(i<20)
 
 print("numero iteraciones = "+str(i))
 '''
-mostrar()
+mostrar(caja)
