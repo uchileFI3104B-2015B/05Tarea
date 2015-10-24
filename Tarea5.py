c0bfcf86745bde3fdeafcc3a3168c164134a8542
maxidirk <maxidirk@yahoo.es>
2015-10-24 17:18:36 -0300

    se construye caja correctamente. se intenta definir el centro de caja

--- Tarea5.py
+++ Tarea5.py
@@ -22,7 +22,17 @@ import matplotlib.pyplot as pyplot
 caja de 10cmx15cm
 centro de caja = (0,0)
 '''
-caja = np.zeros((10,15))
+Lx = 10.           #[cm] largo de la caja en eje x
+Ly = 15.           #[cm] largo de la caja en eje y
+N = 150            #numero de pasos a dar
+hx = Lx / (N - 1)  #tamanho del paso en eje x
+hy = Ly / (N - 1)  #tamanho del paso en eje y
+h = 1.0            #[cm] tamanho del paso
+Nx = (Lx / h) + 1  #numero de pasos a dar en eje x
+Ny = (Ly / h) + 1  #numero de pasos a dar en eje y
+
+caja = np.zeros( (Nx , Ny) )  #se construye caja
+caja[Nx/2,Ny/2] = 1
 
 #######################################################
 
