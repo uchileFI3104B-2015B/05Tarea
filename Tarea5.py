d1768a0755c313307d82dd94e3a24f20e16ca48a
maxidirk <maxidirk@yahoo.es>
2015-10-24 16:42:34 -0300

    se crea archivo .py para iniciar tarea

---
+++ Tarea5.py
@@ -0,0 +1,9 @@
+#######################################################
+'''
+Metodos Numericos para la Ciencia e Ingenieria
+FI3104-1
+Tarea 5
+Maximiliano Dirk Vega Aguilera
+18.451.231-9
+'''
+#######################################################
