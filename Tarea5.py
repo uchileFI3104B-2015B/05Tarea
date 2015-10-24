b67086d5959c87ebea4caee8f40be62fdfad1220
maxidirk <maxidirk@yahoo.es>
2015-10-24 18:04:45 -0300

    se construye caja donde va la letra. se intenta ubicarla correctamente dentro de la caja principal. intentos infructuosos

--- Tarea5.py
+++ Tarea5.py
@@ -24,7 +24,7 @@ centro de caja = (0,0)
 '''
 Lx = 10.           #[cm] largo de la caja en eje x
 Ly = 15.           #[cm] largo de la caja en eje y
-h = 0.5            #[cm] tamanho del paso
+h = 1            #[cm] tamanho del paso
 Nx = (Lx / h) + 1  #numero de pasos a dar en eje x
 Ny = (Ly / h) + 1  #numero de pasos a dar en eje y
 
@@ -54,10 +54,10 @@ Nly = (Lly / h) + 1  #numero de pasos en caja de ltera a dar en eje y
 
 cajal = np.ones( (Nlx , Nly) ) #se construye caja de letra
 
-borde_x_caja1 = (Lx / 2 - Llx) / h
-borde_y_caja1 = (Ly / 2 - Lly) / h
-borde_x_caja2 = (Lx / 2 + Llx) / h
-borde_y_caja2 = (Ly / 2 + Lly) / h
+borde_x_caja1 = ((Lx / 2 + 1) - (Llx / 2 + 1)) / h
+borde_y_caja1 = ((Ly / 2 + 1) - (Lly / 2 + 1)) / h
+borde_x_caja2 = ((Lx / 2 + 1) + (Llx / 2 + 1)) / h
+borde_y_caja2 = ((Ly / 2 + 1) + (Lly / 2 + 1)) / h
 
 caja[borde_x_caja1 : borde_x_caja2 , borde_y_caja1 : borde_y_caja2] = cajal
 
@@ -75,5 +75,6 @@ print borde_y_caja2
 #caja[(Nx-1)/2,(Ny-1)/2] = 1
 
 print np.transpose(caja)
+print np.transpose(cajal)
 
 #######################################################
