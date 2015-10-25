507e267d3ccf7992016707ea39c0f0452cc918b1
maxidirk <maxidirk@yahoo.es>
2015-10-25 02:04:32 -0300

    se crean funciones para hacer lo que se estaba haciendo

--- Tarea5.py
+++ Tarea5.py
@@ -25,15 +25,17 @@ def crea_caja(Lx,Ly,h):
     return caja
 
 
-def letra_M(caja, h, rho):
+def letra_M(Lx, Ly, h, rho):
     '''
-    construye letra M densidad de carga rho en caja de paso h
+    construye letra M densidad de carga rho
+    en caja de largo Lx x Ly y con paso h
     '''
-    Llx = len(cajal[:][0])              #largo caja de letra eje x
-    Lly = len(cajal[0][:])              #largo caja de letra eje y
-    Nlx = (Llx / h) + 1  #numero de pasos en caja de letra a dar en eje x
-    Nly = (Lly / h) + 1  #numero de pasos en caja de ltera a dar en eje y
+    Llx = Lx              #largo caja de letra eje x
+    Lly = Ly              #largo caja de letra eje y
+    Nlx = (Llx / h) + 1   #numero de pasos en caja de letra a dar en eje x
+    Nly = (Lly / h) + 1   #numero de pasos en caja de ltera a dar en eje y
     N_pasos_1cm = 1. / h  #numero de pasos para tener 1 cm
+
     cajal = crea_caja(Llx, Lly, h)
     cajal[0 : 0 + N_pasos_1cm , : ] = rho
     cajal[Nlx - N_pasos_1cm : Nlx , : ] = rho
@@ -43,19 +45,72 @@ def letra_M(caja, h, rho):
     2 * N_pasos_1cm : 3 * N_pasos_1cm] = rho
 
     cajal[Nlx / 2 - N_pasos_1cm / 2 : Nlx / 2 + N_pasos_1cm / 2 + 1 ,
-    N_pasos_1cm : 2 * N_pasos_1cm ] =
-    caja[Nlx / 2 - N_pasos_1cm / 2 : Nlx / 2 + N_pasos_1cm / 2 + 1 ,
-    N_pasos_1cm : 2 * N_pasos_1cm ]
+    N_pasos_1cm : 2 * N_pasos_1cm ] = caja[Nlx / 2 - N_pasos_1cm / 2 :
+     Nlx / 2 + N_pasos_1cm / 2 + 1 , N_pasos_1cm : 2 * N_pasos_1cm ]
 
     return cajal
 
-def asignar_letra(caja,cajal):
+def asignar_letra(caja, Lx , Ly, cajal, Llx, Lly, h):
     '''
     asigna la letra a la caja principal
-    caja = caja principal
-    cajal = caja donde esta la letra
+    caja principal caja Lx x Ly
+    caja de letra cajal Llx x Lly
+    paso h
     '''
-    pass
+    Nx = (Lx / h) + 1  #numero de pasos a dar en eje x
+    Ny = (Ly / h) + 1  #numero de pasos a dar en eje y
+    Nlx = (Llx / h) + 1   #numero de pasos en caja de letra a dar en eje x
+    Nly = (Lly / h) + 1   #numero de pasos en caja de ltera a dar en eje y
+
+    bordex_caja = round((Nx / 2 ) - (Nlx / 2 ))
+    bordey_caja = round((Ny / 2 ) - (Nly / 2 ))
+
+    ly = int(round(Nlx))
+    lx = int(round(Nly))
+
+    try:
+        lx = int(round(Nly))
+        try:
+            ly = int(round(Nlx))
+            for i in range(ly):
+                for j in range(lx):
+                    if cajal[i][j] != 0:
+                        if cajal[i][j] != 0:
+                            caja[bordex_caja : bordex_caja + Nlx ,
+                            bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
+        except:
+            ly = int(round(Nlx)) -1
+            for i in range(ly):
+                for j in range(lx):
+                    if cajal[i][j] != 0:
+                        if cajal[i][j] != 0:
+                            caja[bordex_caja : bordex_caja + Nlx ,
+                            bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
+    except:
+        lx = int(round(Nly)) - 1
+        try:
+            ly = int(round(Nlx))
+            for i in range(ly):
+                for j in range(lx):
+                    if cajal[i][j] != 0:
+                        if cajal[i][j] != 0:
+                            caja[bordex_caja : bordex_caja + Nlx ,
+                            bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
+        except:
+            ly = int(round(Nlx)) -1
+            for i in range(ly):
+                for j in range(lx):
+                    if cajal[i][j] != 0:
+                        if cajal[i][j] != 0:
+                            caja[bordex_caja : bordex_caja + Nlx ,
+                            bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
+
+
+    #asignar la caja entera a la caja principal
+    #caja[bordex_caja : bordex_caja + Nlx ,
+    #bordey_caja : bordey_caja + Nly] = cajal
+
+    return caja
 
 #######################################################
 
@@ -70,7 +125,7 @@ esquina de caja = (0,0)
 '''
 Lx = 10.           #[cm] largo de la caja en eje x
 Ly = 15.           #[cm] largo de la caja en eje y
-h = 0.9            #[cm] tamanho del paso
+h = 1            #[cm] tamanho del paso
 
 caja = crea_caja(Lx, Ly, h)  #se construye caja
 
@@ -79,87 +134,22 @@ caja = crea_caja(Lx, Ly, h)  #se construye caja
 #construir letra
 '''
 dentro de caja de 5cmx7cm
-construir letra M (con h = 0.25 o 0.3 o 0.5 queda bien)
+construir letra M
 grosor 1cm
 '''
-Llx = 5              #largo caja de letra eje x
-Lly = 7              #largo caja de letra eje y
-rho = 1 #densidad de carga
-cajal = crea_caja(Llx, Lly, h) #se construye caja de letra
-cajal = letra_M(cajal, h, rho)
+Llx = 5       #largo caja de letra eje x
+Lly = 7       #largo caja de letra eje y
+rho = 1       #densidad de carga
+
+cajal = crea_caja(Llx, Lly, h)    #se construye caja de letra
+cajal = letra_M(Llx, Lly, h, rho) #se construye letra M en la caja
+
 '''
-Construccion de la letra M
-
-cajal[0 : 0 + N_pasos_1cm , : ] = rho
-cajal[Nlx - N_pasos_1cm : Nlx , : ] = rho
-cajal[ : , N_pasos_1cm : 2 * N_pasos_1cm] = rho
-
-cajal[Nlx / 2 - N_pasos_1cm / 2 : Nlx / 2 + N_pasos_1cm / 2 + 1 ,
-2 * N_pasos_1cm : 3 * N_pasos_1cm] = rho
-
-cajal[Nlx / 2 - N_pasos_1cm / 2 : Nlx / 2 + N_pasos_1cm / 2 + 1 ,
-N_pasos_1cm : 2 * N_pasos_1cm ] = 0
-
-
 asignacion de la letra a la caja principal
-''''''
-busca el centro? creo que no
-caja[Nx/2,Ny/2] = 1
-if Nx % 2 == 0:
-    caja[(Nx / 2) - 1 ,Ny / 2] = 1
-if Ny % 2 == 0:
-    caja[Nx / 2 ,(Ny / 2) - 1] = 1
 '''
-#bordex_caja = round((Nx / 2 ) - (Nlx / 2 ))
-#bordey_caja = round((Ny / 2 ) - (Nly / 2 ))
 
-#ly = int(round(Nlx))
-#lx = int(round(Nly))
-'''
-try:
-    lx = int(round(Nly))
-    try:
-        ly = int(round(Nlx))
-        for i in range(ly):
-            for j in range(lx):
-                if cajal[i][j] != 0:
-                    if cajal[i][j] != 0:
-                        caja[bordex_caja : bordex_caja + Nlx ,
-                        bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
-    except:
-        ly = int(round(Nlx)) -1
-        for i in range(ly):
-            for j in range(lx):
-                if cajal[i][j] != 0:
-                    if cajal[i][j] != 0:
-                        caja[bordex_caja : bordex_caja + Nlx ,
-                        bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
-except:
-    lx = int(round(Nly)) - 1
-    try:
-        ly = int(round(Nlx))
-        for i in range(ly):
-            for j in range(lx):
-                if cajal[i][j] != 0:
-                    if cajal[i][j] != 0:
-                        caja[bordex_caja : bordex_caja + Nlx ,
-                        bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
-    except:
-        ly = int(round(Nlx)) -1
-        for i in range(ly):
-            for j in range(lx):
-                if cajal[i][j] != 0:
-                    if cajal[i][j] != 0:
-                        caja[bordex_caja : bordex_caja + Nlx ,
-                        bordey_caja : bordey_caja + Nly][i][j] = cajal[i][j]
+caja = asignar_letra(caja, Lx, Ly, cajal, Llx, Lly ,h)
 
-
-
-asignar la caja entera a la caja principal
-
-caja[bordex_caja : bordex_caja + Nlx ,
-bordey_caja : bordey_caja + Nly] = cajal
-'''
 #######################################################
 
 #construir condiciiones
