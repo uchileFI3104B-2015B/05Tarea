e46631df6147bd3ee59110291908c7a46814af3d
nicokurtovic <nicokurtovic@gmail.com>
2015-10-26 14:26:26 -0300

    Se inicializa el código

---
+++ poisson2d.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+#                                    TAREA 5                                   #
+################################################################################
+
+'''
+Universidad de Chile
+Facultad de Ciencias Fisicas y Matematicas
+Departamento de Fisica
+FI3104 Metodos Numericos para la Ciencia y la Ingenieria
+Semestre Primavera 2015
+
+Nicolas Troncoso Kurtovic
+'''
+
+import matplotlib.pyplot as     p
+import numpy             as     np
+from   scipy.integrate   import ode
+from   scipy.optimize    import root
+from   scipy.optimize    import curve_fit
+
+################################################################################
+#                                                                              #
+################################################################################
+
+
