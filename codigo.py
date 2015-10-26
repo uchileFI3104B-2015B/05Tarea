029746623b8d57cac1a7d52791c3327a89e6377f
Camila Castillo <camicastillopinto@gmail.com>
2015-10-26 20:46:42 -0300

    Se mueve 1iteracion a funcion
    Se implementa la funcion de 1 iteracion y luego se itera sobre ella 10
    veces, probando el codigo. Ademas se grafica.

--- codigo.py
+++ codigo.py
@@ -24,6 +24,49 @@ def rho(i, j, h):
     else:
         return rho_blanco
 
+def una_iteracion(V, V_next, N_pasosx, N_pasosy, h, w=1.):
+    #Una iteracion
+    #Iteracion Caja completa
+    for i in range(1, int(N_pasosx) - 1):
+        #Debajo de la linea
+        for j in range(1,12):
+            V_next[i, j] = ((1 - w) * V[i, j] +
+                              w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j+1] + V_next[i, j-1] +
+                                       h**2 * rho(i, j, h)))
+        #Encima de la linea
+        for j in range(14, int(N_pasosy) - 1):
+            V_next[i, j] = ((1 - w) * V[i, j] +
+                              w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j+1] + V_next[i, j-1] +
+                                       h**2 * rho(i, j, h)))
+    for j in range(12,14):
+        #A la izquierda de la linea
+        for i in range(1,11):
+            V_next[i, j] = ((1 - w) * V[i, j] +
+                              w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j+1] + V_next[i, j-1] +
+                                       h**2 * rho(i, j, h)))
+        #Derecha de la linea
+        for i in range(41,int(N_pasosx)-1):
+            V_next[i, j] = ((1 - w) * V[i, j] +
+                              w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j+1] + V_next[i, j-1] +
+                                       h**2 * rho(i, j, h)))
+
+    #Iteracion condicion derivativa
+    for i in range(11,41):
+        #Puntos debajo de la linea: -1
+        for j in range(12,13):
+            V_next[i,j] = ((1 - w) * V[i, j] +
+                              w / 3 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j-1] + h**2 * rho(i, j, h) + h*(-1.)))
+        #Puntos sobre la linea: +1
+        for j in range(13,14):
+            V_next[i,j] = ((1 - w) * V[i, j] +
+                              w / 3 * (V[i+1, j] + V_next[i-1, j] +
+                                       V[i, j-1] + h**2 * rho(i, j, h) + h*(1.)))
+
 
 #Main
 
@@ -40,44 +83,37 @@ V = np.zeros((N_pasosx, N_pasosy))
 V_next= np.zeros((N_pasosx, N_pasosy))
 
 
-#Una iteracion
-#Iteracion Caja completa
-for i in range(1, int(N_pasosx) - 1):
-    #Debajo de la linea
-    for j in range(1,12):
-        V_next[i, j] = ((1 - w) * V[i, j] +
-                          w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j+1] + V_next[i, j-1] +
-                                   h**2 * rho(i, j, h)))
-    #Encima de la linea
-    for j in range(14, int(N_pasosy) - 1):
-        V_next[i, j] = ((1 - w) * V[i, j] +
-                          w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j+1] + V_next[i, j-1] +
-                                   h**2 * rho(i, j, h)))
-for j in range(12,14):
-    #A la izquierda de la linea
-    for i in range(1,11):
-        V_next[i, j] = ((1 - w) * V[i, j] +
-                          w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j+1] + V_next[i, j-1] +
-                                   h**2 * rho(i, j, h)))
-    #Derecha de la linea
-    for i in range(41,int(N_pasosx)-1):
-        V_next[i, j] = ((1 - w) * V[i, j] +
-                          w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j+1] + V_next[i, j-1] +
-                                   h**2 * rho(i, j, h)))
+#Iteracion
+una_iteracion(V, V_next, N_pasosx, N_pasosy, h, w=1.)
+counter = 1
+while counter < 10: #and no_ha_convergido(phi, phi_next, tolerancia=1e-7):
+    V = V_next.copy()
+    una_iteracion(V, V_next, N_pasosx, N_pasosy, h, w=1.)
+    counter += 1
 
-#Iteracion condicion derivativa
-for i in range(11,41):
-    #Puntos debajo de la linea: -1
-    for j in range(12,13):
-        V_next[i,j] =  ((1 - w) * V[i, j] +
-                          w / 3 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j-1] + h**2 * rho(i, j, h) + h*(-1.)))
-    #Puntos sobre la linea: +1
-    for j in range(13,14):
-        V_next[i,j] =  ((1 - w) * V[i, j] +
-                          w / 3 * (V[i+1, j] + V_next[i-1, j] +
-                                   V[i, j-1] + h**2 * rho(i, j, h) + h*(1.)))
+print("counter = {}".format(counter))
+
+
+V_next_traspuesta=V_next.transpose()
+
+
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-1, 1, N_pasosx)
+y = np.linspace(-1, 1, N_pasosy)
+
+X, Y = np.meshgrid(x, y)
+
+ax.plot_surface(X, Y, V_next_traspuesta, rstride=1, cstride=1)
+fig.show()
+
+fig2 = plt.figure(2)
+fig2.clf()
+ax2 = fig2.add_subplot(111)
+ax2.imshow(V_next_traspuesta, origin='bottom', interpolation='nearest')
+ax2.contour(V_next_traspuesta, origin='lower')
+fig2.show()
+
+plt.draw()
