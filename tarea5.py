40e15bdcca111a6934303d049af7055cabdd51a5
Fernanda Perez Verdugo <fer20mc@gmail.com>
2015-10-26 14:29:17 -0300

    se inicializa set up

---
+++ tarea5.py
@@ -0,0 +1,21 @@
+'''
+Este script...
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+
+#Main
+#Setup
+
+Lx = 10
+Ly = 15
+h = 0.2
+N_pasos_x = int(Lx/h +1)
+N_pasos_y = int(Ly/h +1)
+
+v = np.zeros((N_pasos_x, N_pasos_y))
+v_next = np.zeros((N_pasos_x, N_pasos_y))
