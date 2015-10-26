2ea50e4aca659c91d8e4901da7dbac08697ab2de
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-26 13:03:16 -0400

    inicializacion

---
+++ intento1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''
+Programa que busca integrar la ecuacion de poisson en 2D
+'''
+#librerias
+
+
+#funciones estructurales
+
+
+
+#condiciones de borde y/o iniciales
+
+Lx = 10
+Ly = 15
+N_x = 10
+N_y = 15
+
+v = np.zeros((N_x, N_y))
+v_s = np.zeros((N_x, N_y))
+
+
+#iteracion
+
+
+
+#graficos (?)
