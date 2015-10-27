50c5cbbbb73c55fad53bd84c3962d6d81b74cc8c
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-10-27 01:12:30 -0300

    cambios menores

--- tarea5.py
+++ tarea5.py
@@ -7,11 +7,9 @@ densidad=1/19.
 h=0.2 #paso, en centímetros
 L_x=10 #ancho de la grilla, está en centímetros
 L_y=15 #alto de la grilla, está en centímetros
-Steps_x=int((L_x/h)+1)
-Steps_y=int((L_y/h)+1)
-
-def show(phi):
-    print(phi[::-1, :])
+Steps_x=int((L_x/h)+1) #número de pasos en X
+Steps_y=int((L_y/h)+1) #número de pasos en Y
+omega=1.2
 
 def rho(i, j, h):
     x=i*h-(L_x/2.) #los x e y están en centímetros, el origen está en el centro de la grilla
@@ -22,14 +20,14 @@ def rho(i, j, h):
         return densidad
     elif (x>=-2.5 and x<=2.5) and (y>=2.5 and y<=3.5): #palito de arriba de la E
         return densidad
-    elif (x>=-2.5 and x<=-1.5) and (y>=-2.5 and y<=-0.5): #segmento entre palito de abajo y el de al medio
+    elif (x>=-2.5 and x<=-1.5) and (y>=-2.5 and y<=-0.5): #segmento entre palito de abajo y el del medio
         return densidad
     elif (x>=-2.5 and x<=-1.5) and (y>=0.5 and y<=2.5): #segmento entre palito del medio y el de arriba
         return densidad
     else:
         return 0
 
-def una_iteracion(v,v_next,Steps_x,Steps_y,h,w=1.): #aquí hay que separar para 3 casos: borde, línea y el resto
+def una_iteracion(v,v_next,Steps_x,Steps_y,h,w=omega): #aquí hay que separar para 2 casos: la línea que es C.B. y el resto
     for i in range(1,Steps_x-1):
         for j in range(1,12): #abajo de la línea
             v_next[i,j]=((1-w)*v[i,j]+(w/4.)*(v[i+1,j]+v_next[i-1,j]+v[i,j+1]+v_next[i,j-1]+h**2*rho(i,j,h)))
@@ -46,29 +44,40 @@ def una_iteracion(v,v_next,Steps_x,Steps_y,h,w=1.): #aquí hay que separar para 
         for j in range(13,14): #justo sobre la línea, donde aplica la condición +1
             v_next[i,j]=((1-w)*v[i,j]+(w/3.)*(v[i+1,j]+v_next[i-1,j]+v_next[i,j-1]+h**2*rho(i,j,h)+h))
 
-V=np.zeros((Steps_x,Steps_y))
+def no_ha_convergido(v,v_next,tol=1e-4):
+    not_zero=(v_next!=0)
+    diff_relativa=(v-v_next)[not_zero]/v_next[not_zero]
+    max_diff=np.max(np.fabs(diff_relativa))
+    if max_diff>tol:
+        return True
+    else:
+        return False
+
+V=np.zeros((Steps_x,Steps_y)) #genero grillas llenas con ceros, esto me da por garantizado que en el borde V=0
 V_next=np.zeros((Steps_x,Steps_y))
 
-una_iteracion(V,V_next,Steps_x,Steps_y,h,w=1.)
+una_iteracion(V,V_next,Steps_x,Steps_y,h,w=omega)
 counter=1
-while counter<10:
+while counter<1000 and no_ha_convergido(V,V_next,tol=1e-4):
     V=V_next.copy()
-    una_iteracion(V,V_next,Steps_x,Steps_y,h,w=0.8)
-    counter+= 1
-Vnext_alverre=V_next.transpose()
+    una_iteracion(V,V_next,Steps_x,Steps_y,h,w=omega)
+    counter+=1
+print("counter={}".format(counter))
+V_trans=V_next.transpose()
+
 fig=plt.figure(1)
 fig.clf()
 ax=fig.add_subplot(111,projection='3d')
 x=np.linspace(-1,1,Steps_x)
 y=np.linspace(-1,1,Steps_y)
 X,Y=np.meshgrid(x,y)
-ax.plot_surface(X,Y,Vnext_alverre,rstride=1,cstride=1)
-fig.show()
+ax.plot_surface(X,Y,V_trans,rstride=1,cstride=1)
 plt.savefig('plot_surface.png')
+fig.show()
 
 fig2=plt.figure(2)
 fig2.clf()
 ax2=fig2.add_subplot(111)
-ax2.imshow(Vnext_alverre,origin='bottom',interpolation='nearest')
-fig2.show()
+ax2.imshow(V_trans,origin='bottom',interpolation='nearest')
 plt.savefig('plot_imshow.png')
+fig2.show()
