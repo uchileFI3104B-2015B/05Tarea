d11fb6ca872b66ceeebe387e6f80c995aa3ca885
hpozojose <hpozo.jose@gmail.com>
2015-10-27 00:29:27 -0300

    agrega mas subareas

--- Poisson.py
+++ Poisson.py
@@ -20,10 +20,11 @@ def muestra_phi(phi):
     pass
 
 
-def DensidadLetra(i, j, h):
+def Rho_Letra(i, j, h):
     '''
     Pide coordenadas de i y j (posicion) y le asigna el valor de la densidad
-    proporcianado por la ecuacion de poisson y Laplace en el cuadrado de la letra
+    proporcianado por la ecuacion de poisson y Laplace en el cuadrado de la
+    letra
     '''
     #primero centramos las coordenadas
     x = i * h - 5
@@ -34,22 +35,72 @@ def DensidadLetra(i, j, h):
         return value
     if y>=-3.5 and y<=3.5 and x>=1.5 and x<=2.5:
         return value
-    if y>=0.5 and y<=2.5 and x>=-1.5 and x<=-0.5:
+    if y>=0.5 and y<=2.5 and x>-1.5 and x<=-0.5:
         return value
-    if y>=0.5 and y<=2.5 and x>=0.5 and x<=1.5:
+    if y>=0.5 and y<=2.5 and x>=0.5 and x<1.5:
             return value
-    if y>=-0.5 and y<=1.5 and x>=-0.5 and x<=0.5:
+    if y>=-0.5 and y<=1.5 and x>-0.5 and x<0.5:
         return value
     else:
         return 0
-'''
-def una_iteracion(phi, phi_next, N_pasos, h, w=1.):
-    for i in range(1, N_pasos-1):
-        for j in range(1, N_pasos-1):
+
+
+def una_iteracion(phi, phi_next, Nx_pasos, Ny_pasos, h, w=1.):
+    #Parte Izquierda del area, la cual no contiene la letra
+    for i in range(1, 10):
+        for j in range(1, Ny_pasos - 1):
             phi_next[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
-                                       phi[i, j+1] + phi_next[i, j-1] +
-                                       h**2 * q(i, j, h)))
+                                       phi[i, j+1] + phi_next[i, j-1] #+
+                                     #  h**2 * Rho_Letra(i, j, h)))
+    #Parte Derecha del area, tampoco tiene letra
+    for i in range(40, Nx_pasos - 1):
+        for j in range(1, Ny_pasos-1):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1] #+
+                                       #h**2 * Rho_Letra(i, j, h)))
+    #Parte Central Superior
+    for i in range(10, 40)
+        for j in range(56, Ny_pasos - 1):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1] #+
+                                       #h**2 * Rho_Letra(i, j, h)))
+    #Linea
+    #Parte Central Baja Lejana a la linea por bajo esta esta
+    for i in range(10, 40):
+        for j in range(1, 20):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1] #+
+                                       #h**2 * Rho_Letra(i, j, h)))
+    #Parte Central Baja Lejana a la linea por sobre esta esta
+    for i in range(10, 40):
+        for j in range(20, 27):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1]
+    #Linea en si
+    for i in range(10, 40):
+        phi_next[i, 27] = ((1 - w) * phi[i, 27] + w / 4 * (phi[i+1, 27] + phi_next[i-1, 27] +
+                                   phi[i, 27+1] + phi_next[i, 27-1] #+
+                                       #h**2 * Rho_Letra(i, j, h)))
+    #Linea
+    #Parte Central Baja Lejana a la linea por bajo esta esta
+    for i in range(10, 40):
+        for j in range(1, 20):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1] #+
+                                       #h**2 * Rho_Letra(i, j, h)))
+    #Parte Central Baja Lejana a la linea por sobre esta esta
+    for i in range(10, 40):
+        for j in range(20, 27):
+            phi_next[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_next[i-1, j] +
+                                       phi[i, j+1] + phi_next[i, j-1]
+
 
 
 def no_ha_convergido(phi, phi_next, tolerancia=1e-5):
@@ -66,9 +117,14 @@ def no_ha_convergido(phi, phi_next, tolerancia=1e-5):
 
 # Setup
 
-Lx = Ly = 2
-N_pasos = 21
-h = Lx / (N_pasos - 1)
+Lx = 10
+Ly = 15
+h = 0.2
+N_pasos_x = int(Lx/h +1)
+N_pasos_y = int(Ly/h +1)
+
+v_rho = np.zeros((N_pasos_x, N_pasos_y))
+v_rho_next = np.zeros((N_pasos_x, N_pasos_y))
 
 phi = np.zeros((N_pasos, N_pasos))
 phi_next = np.zeros((N_pasos, N_pasos))
