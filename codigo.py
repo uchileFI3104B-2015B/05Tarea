373aac20500fcef350a78b5dbbf3885b10d52e89
benjammin94 <benja_hunter_x@hotmail.com>
2015-10-27 19:28:11 -0300

    Update 2

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from future import division
 
 Lx=10
 Ly=15
@@ -8,5 +8,24 @@ N_pasos_x = Lx / h + 1
 N_pasos_y = Ly / h + 1
 
 phi=np.zeros( ( N_pasos_x , N_pasos_y ) )
+
+def rho(i, j, h):
+    x = i * h - 5
+    y = j * h - 7.5
+    rho_letra = 1 / 23
+    rho_blanco = 0
+
+    if x >= -2.5 and x <= -1.5 and y >= -3.5 and y <= 3.5:
+        return rho_letra
+    if x >= 1.5 and x <= 2.5 and y >= -3.5 and y <= 3.5:
+        return rho_letra
+    if y <= 3.5 and x >= 2.5 and x > -1.5 and x < 1.5:
+        return rho_letra
+    if y <= 0.5 and y >= -0.5 and x > -1.5 and x < 1.5:
+        return rho_letra
+    if y <= -2.5 and y >= -3.5 and x > -1.5 and x < 1.5:
+        return rho_letra
+    else:
+        return rho_blanco
 
 print phi
