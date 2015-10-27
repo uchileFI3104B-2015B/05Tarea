4686a07eef18d6129d025d587713e9b123e21fca
TatiFlores <tatiflores.4@gmail.com>
2015-10-27 09:59:30 -0300

    Escrita la funcion rho

--- tarea5.py
+++ tarea5.py
@@ -4,10 +4,40 @@
 '''
 Aquí va la descripcion de mi tarea
 '''
+densdad = 1. / 20.
+Lx = 10
+Ly = 15
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def rho(i, j, h):
+    x = i * h - Lx / 2. #La resta es para dejar el eje al centro
+    y = j * h - Ly / 2.
+    if (x>=-2.5 and x <= 2.5) and (y>=-3.5 and y <= 3.5):
+        #Esta condicion es para entrar a la caja
+        if (x >= -2.5 and  x <= -1.5) or (x >= 1.5 and x <= 2.5):
+            #Lineas a los lados de M
+            return densidad
+
+        elif (x>=-1.5 and x <= -0.5) and (y>=0.5 and y >=2.5):
+            #Barrita a la izquierda
+            return densidad
+
+        elif (x>=0.5 and x <= 1.5) and (y>=0.5 and y >=2.5):
+            #Barrita a la derecha
+            return densidad
+
+        elif (x >= -0.5 and x <= 0.5) and (y >= -0.5 and y <= 1.5):
+            #Barrita al medio
+            return densidad
+
+        else:
+            return 0 #Dentro de la caja pero no es la letra M
+
+    else:
+        return 0 #Fuera de la caja
 
 def una_iteracion(V, V_next, N_pasos, h, w):
     pass
@@ -22,8 +52,7 @@ def no_ha_convergido(V, V_next, tolerancia):
 
 # Setup
 
-Lx = 10
-Ly = 15
+
 h = 0.2
 N_steps_x = Lx / h
 N_steps_y = Ly / h
