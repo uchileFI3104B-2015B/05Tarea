c842ccc9ce7e8f77286e5ad19e7cd319f6b033fe
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-10-27 20:29:21 -0300

    Se crean funciones auxiliares para crear la caja, sus condiciones de borde, y transformar coordendadas en cm a saltos de h.

--- Poisson.py
+++ Poisson.py
@@ -10,6 +10,36 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+import pdb
+
+def crear_caja(x, y, h):
+    '''Recibe las dimensiones de la caja  y el tamaño del reticulado
+     y la inicia en puros ceros '''
+    ancho_efectivo = x / h + 1
+    alto_efectivo = y / h + 1
+    caja = np.zeros((ancho_efectivo, alto_efectivo))
+
+    return caja
+
+def poner_condiciones_borde(caja):
+    '''conecta a tierra el perímetro de la caja, la segunda iteración
+    (bordes laterales) se itera desde borde+1, hasta borde para evitar pasar
+    2 veces por las esquinas'''
+    borde_inferior = np.array([np.array([-5, -7.5]), np.array([5, -7.5])])
+    borde_superior = np.array([np.array([-5, 7.5]), np.array([5, 7.5])])
+    borde_inferior[0] = transformar(borde_inferior[0])
+    borde_inferior[1] = transformar(borde_inferior[1])
+    borde_superior[0] = transformar(borde_superior[0])
+    borde_superior[1] = transformar(borde_superior[1])
+    for i in range(int(borde_inferior[0][0]), int(borde_inferior[1][0]) + 1):
+        caja[i, borde_inferior[0][1]] = 0
+        caja[i, borde_superior[0][1]] = 0
+    for i in range(int(borde_inferior[0][1])+1, int(borde_superior[0][1])):
+        caja[borde_inferior[0][0], i] = 0
+        caja[borde_superior[1][0], i] = 0
+    return caja
+    
+
 
 def dentro_letra_B(x, y, h):
     
