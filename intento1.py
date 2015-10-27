bf381f546d872f6994f8d7f027d2d24e333dc8cd
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-26 21:18:57 -0400

    1er debugging
    se corrigen los buggs y se prueba como se ve el potencial en la grilla.
    no se considera la condicion de la linea con potencial diferente aun y
    queda pendiente optimizar la iteración al definir por partes
    por alguna razon el grafico se abre y se cierra de inmediato

--- intento1.py
+++ intento1.py
@@ -20,21 +20,32 @@ def r(i, j, Lx, Ly, h):
     dx = 2.5
     dy = 3.5
     l = 1
-    rho = 1/11
+    rho = 1/11.
+    r = 0
     if x >= -dx and x <= -dx + l:
         if y >= -dy and y <= dy:
-            return rho
+            r = rho
     elif x >= l - dx and x <= dx:
         if y >= -dy and y <= -dy +l:
-            return rho
-    else:
-        return 0
+            r = rho
+    return r
 
 
-def una_iter(phi, phi_n, N_x, N_y, h, w = 1.):
+def una_iter(phi, phi_n, N_x, N_y, Lx, Ly, h, w = 1.):
     '''
     Da un paso en la iteracion para toda la grilla
     '''
+    N_x = int(N_x)
+    N_y = int(N_y)
+    for i in range(1, N_x - 1):
+        for j in range(1, N_y - 1):
+            phi_n[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
+                                       phi[i, j+1] + phi_n[i, j-1] +
+                                       h**2. * r(i, j, Lx, Ly, h)))
+
+
+
     pass
 
 
@@ -42,10 +53,10 @@ def no_ha_convergido(phi, phi_n, toler = 1e-5):
     '''
     ve si la funcion ya ha convergido
     '''
-    not_zero = (phi_next != 0)
+    not_zero = (phi_n != 0)
     res = (phi - phi_n)[not_zero]/ phi_n[not_zero]
     max_res = np.max(np.fabs(res))
-    if max_diff > tolerancia:
+    if max_res > toler:
         return True
     else:
         return False
@@ -61,19 +72,21 @@ w = 1
 N_x = Lx/ h
 N_y = Ly/ h
 
-v = np.zeros((N_x, N_y))
-v_s = np.zeros((N_x, N_y))
+
+phi = np.zeros((N_x, N_y))
+phi_s = np.zeros((N_x, N_y))
 
 
 #iteracion
 
-una_iter(phi, phi_n, N_x, N_y, h, w)
+una_iter(phi, phi_s, N_x, N_y, Lx, Ly, h, w)
 n = 1
-Nf = 800
-while n < Nf and no_ha_convergido(phi, phi_n):
-    phi = phi_n.copy()
-    una_iter(phi, phi_n, N_x, N_y, h, w)
-    n += 1
+Nf = 100
+while n < Nf and no_ha_convergido(phi, phi_s):
+    phi = phi_s.copy()
+    una_iter(phi, phi_s, N_x, N_y, Lx, Ly, h, w)
+    n = n + 1
+    print n
 
 
 
@@ -81,3 +94,25 @@ while n < Nf and no_ha_convergido(phi, phi_n):
 
 
 #graficos (?)
+print("contador = {}".format(n))
+'''
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-Lx/2, Lx, N_x)
+y = np.linspace(-Ly/2, Ly, N_y)
+
+X, Y = np.meshgrid(x, y)
+
+ax.plot_surface(X, Y, phi_s, rstride=1, cstride=1)
+fig.show()
+'''
+fig2 = plt.figure(2)
+fig2.clf()
+ax2 = fig2.add_subplot(111)
+ax2.imshow(phi_s, origin='bottom', interpolation='nearest')
+ax2.contour(phi_s, origin='lower')
+fig2.show()
+
+plt.draw()
