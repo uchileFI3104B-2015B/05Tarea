4f4a672e6d314999d9c32336ceac8feb8c5269b1
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-27 13:22:05 -0400

    analisis de w
    se agrega un programa que analiza el comportamiento de los w. además, un
    gráfico para visualizarlo.
    se cambian los ejes del grafico en el programa para que se vea mejor,
    pero no se corre por tiempo

--- analisis_de_w.py
+++ analisis_de_w.py
@@ -14,13 +14,14 @@ from mpl_toolkits.mplot3d import axes3d
 
 #funciones estructurales
 
+
 def r(i, j, Lx, Ly, h):
-    x = i * h - Lx / 2
-    y = j * h - Ly / 2
+    x = i* h - Lx/ 2
+    y = j* h - Ly/ 2
     dx = 2.5
     dy = 3.5
     l = 1
-    rho = 1 / 11.
+    rho = 1/11.
     r = 0
     if x >= -dx and x <= -dx + l:
         if y >= -dy and y <= dy:
@@ -31,34 +32,6 @@ def r(i, j, Lx, Ly, h):
     return r
 
 
-'''
-pendiente una implementacion mas compacta para la iteracion
-def paso_sin_carga(phi, phi_n, N_x, N_y):
-    'solo la iteracion son considerar el valor de la densidad'
-    N_x = int(N_x)
-    N_y = int(N_y)
-    for i in range(1, N_x - 1):
-        for j in range(1, N_y - 1):
-            phi_n[i, j] = ((1 - w) * phi[i, j] +
-                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] ))
-
-
-def paso_con_carga(phi, phi_n, p, nx_i, ny_i, nx_f, ny_f):
-    'solo la iteracion en las zonas con carga'
-    N_x, N_y, Lx, Ly, h, w = p
-    N_x = int(N_x)
-    N_y = int(N_y)
-    for i in range(1, N_x - 1):
-        for j in range(1, N_y - 1):
-            phi_n[i, j] = ((1 - w) * phi[i, j] +
-                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] +
-                                       h**2. * r(i, j, Lx, Ly, h)))
-'''
-
-
-
 def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
     '''
     Da un paso en la iteracion para toda la grilla parte por parte
@@ -66,18 +39,10 @@ def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
     N_x, N_y, Lx, Ly, h, w = p
     N_x = int(N_x)
     N_y = int(N_y)
-    '''
-    for i in range(1, N_x - 1):
-        for j in range(1, N_y - 1):
-            phi_n[i, j] = ((1 - w) * phi[i, j] +
-                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] +
-                                       h**2. * r(i, j, Lx, Ly, h)))
-    '''
     for j in range(1, 12): #abajo de la linea
         for i in range(1, N_x -1): #recorre completo
             phi_n[i, j] = ((1 - w) * phi[i, j] +
-                            w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
+                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
                                        phi[i, j+1] + phi_n[i, j-1] ))
 
     for j in range(12, 14): #en la linea
@@ -92,18 +57,18 @@ def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
         #esta parte es redundante, pero me sirve para ordenar mis ideas
         for j in range(12, 13): #bajo la linea, usando la condicion -1
             for i in range(11, 41):
-                phi_n[i, j] =  ((1 - w) * phi[i, j] +
-                                w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
-                                         phi_n[i, j-1] +
-                                         h**2 * r(i, j, Lx, Ly, h) + h*(-1.)))
+                phi_n[i,j] =  ((1 - w) * phi[i, j] +
+                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
+                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h)
+                                   + h*(-1.)))
 
 
         for j in range(13, 14): #encima de la linea, usando condicion +1
             for i in range(11, 41):
-                phi_n[i, j] =  ((1 - w) * phi[i, j] +
-                                 w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
-                                          phi_n[i, j-1] +
-                                          h**2 * r(i, j, Lx, Ly, h) + h*(1.)))
+                phi_n[i,j] =  ((1 - w) * phi[i, j] +
+                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
+                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h)
+                                   + h*(1.)))
 
 
     for j in range(14, 17): #sobre la linea y antes del rectangulo de la letra
@@ -132,7 +97,6 @@ def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
                                        phi[i, j+1] + phi_n[i, j-1] ))
 
-#    pass
 
 def no_ha_convergido(phi, phi_n, toler = 1e-5):
     '''
@@ -145,62 +109,52 @@ def no_ha_convergido(phi, phi_n, toler = 1e-5):
         return True
     else:
         return False
-#    pass
 
 
-#condiciones de borde y/o iniciales
-
-Lx = 10
-Ly = 15
-h = 0.2
-w = 1.2
-N_x = Lx / h
-N_y = Ly / h
+def n_relajacion(w = 1, N = 5000):
+    Lx = 10
+    Ly = 15
+    h = 0.2
+    N_x = Lx/ h
+    N_y = Ly/ h
 
 
-phi = np.zeros((N_x, N_y))
-phi_s = np.zeros((N_x, N_y))
+    phi = np.zeros((N_x, N_y))
+    phi_s = np.zeros((N_x, N_y))
 
 
-#iteracion
+    #iteracion
 
-p= (N_x, N_y, Lx, Ly, h, w) #mas practico definir el vector para usar como
-                            #parametro
-una_iter(phi, phi_s, p)
-n = 1
-Nf = 3000
-while n < Nf and no_ha_convergido(phi, phi_s):
-    phi = phi_s.copy()
+    p= (N_x, N_y, Lx, Ly, h, w) #mas practico definir el vector para usar como
+                                #parametro
     una_iter(phi, phi_s, p)
-    n = n + 1
-    print n #permite ver si el algoritmo esta avanzando o se queda pegado
+    n = 1
+    while n < N and no_ha_convergido(phi, phi_s):
+        phi = phi_s.copy()
+        una_iter(phi, phi_s, p)
+        n = n + 1
+        print n #permite ver si el algoritmo esta avanzando o se queda pegado
+    return n
+
+
+W = [0.8, 1, 1.2, 1.4, 1.6, 1.8, 2]
+N = []
+for w in W:
+    n = n_relajacion(w)
+    N.append(n)
+print W
+print N
 
 
 
+fig1 = plt.figure(1)
+fig1.clf()
+ax1 = fig1.add_subplot(111)
+ax1.plot(W, N, 'o')
+plt.title('N de relajacion vs $\omega$')
+ax1.set_ylabel('N d relajacion')
+ax1.set_xlabel('$\omega$')
 
-
-
-#graficos (?)
-print("contador = {}".format(n))
-
-phi_fin_trasp = phi_s.transpose()
-
-fig2 = plt.figure(2)
-fig2.clf()
-ax2 = fig2.add_subplot(111)
-ax2.imshow(phi_fin_trasp, origin='bottom', interpolation='nearest')
-ax2.contour(phi_fin_trasp, origin='lower')
-plt.savefig('fig1.png')
-fig2.show()
-
-plt.draw()
-
-fig = plt.figure(1)
-fig.clf()
-ax = fig.add_subplot(111,projection='3d')
-x = np.linspace(-1, 1, N_x)
-y = np.linspace(-1, 1, N_y)
-X, Y = np.meshgrid(x, y)
-ax.plot_surface(X, Y, phi_fin_trasp, rstride=1, cstride=1)
-plt.savefig('fig2.png')
-fig.show()
+plt.savefig('N_relajacion.png')
+fig1.show()
+# [5000, 3727, 2557, 1691, 1011, 442, 5000]
