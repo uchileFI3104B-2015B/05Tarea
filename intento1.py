fa0c2d9617a51bc7cb01798e56d2c57ff564cc33
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-27 02:52:14 -0400

    cambios pequeños
    se realiza una iteracion mas larga con w = 1.2, comprobando que en algun
    momento se cumple la condicion de convergencia

--- intento1.py
+++ intento1.py
@@ -32,6 +32,7 @@ def r(i, j, Lx, Ly, h):
 
 
 '''
+pendiente una implementacion mas compacta para la iteracion
 def paso_sin_carga(phi, phi_n, N_x, N_y):
     'solo la iteracion son considerar el valor de la densidad'
     N_x = int(N_x)
@@ -60,7 +61,7 @@ def paso_con_carga(phi, phi_n, p, nx_i, ny_i, nx_f, ny_f):
 
 def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
     '''
-    Da un paso en la iteracion para toda la grilla
+    Da un paso en la iteracion para toda la grilla parte por parte
     '''
     N_x, N_y, Lx, Ly, h, w = p
     N_x = int(N_x)
@@ -73,56 +74,59 @@ def una_iter(phi, phi_n, p): # = N_x, N_y, Lx, Ly, h, w = 1.
                                        phi[i, j+1] + phi_n[i, j-1] +
                                        h**2. * r(i, j, Lx, Ly, h)))
     '''
-    for j in range(1, 12):
-        for i in range(1, N_x -1):
+    for j in range(1, 12): #abajo de la linea
+        for i in range(1, N_x -1): #recorre completo
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
                                        phi[i, j+1] + phi_n[i, j-1] ))
 
-    for j in range(12, 14):
+    for j in range(12, 14): #en la linea
+        for i in range(1, 11): #para el borde antes de la linea
+            phi_n[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
+                                       phi[i, j+1] + phi_n[i, j-1] ))
+        for i in range(41, N_x - 1): #borde despues de la linea
+            phi_n[i, j] = ((1 - w) * phi[i, j] +
+                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
+                                       phi[i, j+1] + phi_n[i, j-1] ))
+        #esta parte es redundante, pero me sirve para ordenar mis ideas
+        for j in range(12, 13): #bajo la linea, usando la condicion -1
+            for i in range(11, 41):
+                phi_n[i,j] =  ((1 - w) * phi[i, j] +
+                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
+                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h)
+                                   + h*(-1.)))
+
+
+        for j in range(13, 14): #encima de la linea, usando condicion +1
+            for i in range(11, 41):
+                phi_n[i,j] =  ((1 - w) * phi[i, j] +
+                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
+                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h)
+                                   + h*(1.)))
+
+
+    for j in range(14, 17): #sobre la linea y antes del rectangulo de la letra
         for i in range(1, N_x - 1):
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
                                        phi[i, j+1] + phi_n[i, j-1] ))
-
-        for i in range(1, 11):
+    for j in range(17, 58): #a la altura del rectangulo
+        for i in range(1, 12): #a la derecha
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] ))
-        for i in range(41, N_x - 1):
-            phi_n[i, j] = ((1 - w) * phi[i, j] +
-                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] ))
-
-        for j in range(12, 13): #redundante, pero me sirve para ordenar mi idea
-            for i in range(11, 41):
-                phi_n[i,j] =  ((1 - w) * phi[i, j] +
-                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
-                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h) + h*(-1.)))
-
-
-        for j in range(13, 14): #redundante, pero me sirve para ordenar mi idea
-            for i in range(11, 41):
-                phi_n[i,j] =  ((1 - w) * phi[i, j] +
-                          w / 3 * (phi[i+1, j] + phi_n[i-1, j] +
-                                   phi_n[i, j-1] + h**2 * r(i, j, Lx, Ly, h) + h*(1.)))
-
-
-    for j in range(14, 17):
-        for i in range(1, 12):
-            phi_n[i, j] = ((1 - w) * phi[i, j] +
-                              w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] ))
-        for i in range(12, 38):
+                                       phi[i, j+1] + phi_n[i, j-1]))
+        for i in range(12, 38): #dentro
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
                                        phi[i, j+1] + phi_n[i, j-1] +
                                        h**2. * r(i, j, Lx, Ly, h)))
-        for i in range(38, N_x -1):
+        for i in range(38, N_x -1): #a la izq
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
-                                       phi[i, j+1] + phi_n[i, j-1] ))
-    for j in range(17, N_y - 1):
+                                       phi[i, j+1] + phi_n[i, j-1]))
+
+    for j in range(58, N_y - 1): #sobre el rectangulo
         for i in range(1, N_x -1):
             phi_n[i, j] = ((1 - w) * phi[i, j] +
                               w / 4 * (phi[i+1, j] + phi_n[i-1, j] +
@@ -149,11 +153,9 @@ def no_ha_convergido(phi, phi_n, toler = 1e-5):
 Lx = 10
 Ly = 15
 h = 0.2
-w = 1
+w = 1.2
 N_x = Lx/ h
 N_y = Ly/ h
-print N_x
-print N_y
 
 
 phi = np.zeros((N_x, N_y))
@@ -162,15 +164,16 @@ phi_s = np.zeros((N_x, N_y))
 
 #iteracion
 
-p= (N_x, N_y, Lx, Ly, h, w )
+p= (N_x, N_y, Lx, Ly, h, w) #mas practico definir el vector para usar como
+                            #parametro
 una_iter(phi, phi_s, p)
 n = 1
-Nf = 100
+Nf = 3000
 while n < Nf and no_ha_convergido(phi, phi_s):
     phi = phi_s.copy()
     una_iter(phi, phi_s, p)
     n = n + 1
-    print n
+    print n #permite ver si el algoritmo esta avanzando o se queda pegado
 
 
 
