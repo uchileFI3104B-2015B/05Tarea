9d6bc1ef01d3738969df8d960f3b7220aa8b8269
Martín!!! <martin_panza_p@hotmail.com>
2015-10-28 00:52:43 -0300

    Se realizan las modificaciones PEP8

--- Tarea5.py
+++ Tarea5.py
@@ -11,55 +11,57 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
 
-def muestra_phi(phi):
-    print(phi[::-1, :])
-
-def en_letra(i,j,Letra):
-    if Letra.count((i,j))!=0:
+def en_letra(i, j, Letra):
+    if Letra.count((i, j)) != 0:
         return True
     else:
         return False
 
-def en_linea(i,j, Linea):
-    if Linea.count((i,j))!=0:
+
+def en_linea(i, j, Linea):
+    if Linea.count((i, j)) != 0:
         return True
     else:
         return False
 
-def arriba_linea(i,j, arLinea):
-    if arLinea.count((i,j))!=0:
+
+def arriba_linea(i, j, arLinea):
+    if arLinea.count((i, j)) != 0:
         return True
     else:
         return False
-        
-def abajo_linea(i,j, abLinea):
-    if abLinea.count((i,j))!=0:
+
+
+def abajo_linea(i, j, abLinea):
+    if abLinea.count((i, j)) != 0:
         return True
     else:
         return False
-    
-def una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h, Letra,Ab_linea,Ar_linea,w=1.):
+
+
+def una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h,
+                  Letra, Ab_linea, Ar_linea, w=1.):
     for i in range(1, Nx_pasos-1):
             for j in range(1, Ny_pasos-1):
-                if arriba_linea(i,j,Ar_linea):
+                if arriba_linea(i, j, Ar_linea):
                     V_next[i, j] = ((1 - w) * V[i, j] +
-                                  w / 3 * (V[i+1, j] + V_next[i-1, j] +
-                                           V[i, j-1] + h))
-                
-                elif abajo_linea(i,j,Ab_linea):
+                                    w / 3 * (V[i+1, j] + V_next[i-1, j] +
+                                    V[i, j-1] + h))
+
+                elif abajo_linea(i, j, Ab_linea):
                     V_next[i, j] = ((1 - w) * V[i, j] +
-                                  w / 3 * (V[i+1, j] + V_next[i-1, j] +
-                                           V[i, j-1] - h))
-        
-                elif en_letra(i,j,Letra):
+                                    w / 3 * (V[i+1, j] + V_next[i-1, j] +
+                                    V[i, j-1] - h))
+
+                elif en_letra(i, j, Letra):
                     V_next[i, j] = ((1 - w) * V[i, j] +
-                                  w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                           V[i, j+1] + V_next[i, j-1] -
-                                           1./20.))
+                                    w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                    V[i, j+1] + V_next[i, j-1] -
+                                      1. / 20.))
                 else:
                     V_next[i, j] = ((1 - w) * V[i, j] +
-                                  w / 4 * (V[i+1, j] + V_next[i-1, j] +
-                                           V[i, j+1] + V_next[i, j-1]))
+                                    w / 4 * (V[i+1, j] + V_next[i-1, j] +
+                                    V[i, j+1] + V_next[i, j-1]))
 
 
 def no_ha_convergido(V, V_next, tolerancia=1e-5):
@@ -78,53 +80,55 @@ def no_ha_convergido(V, V_next, tolerancia=1e-5):
 
 Lx = 10.
 Ly = 15.
-h=0.2
-Nx_pasos = int(Lx/h+1)
-Ny_pasos = int(Ly/h+1)
+h = 0.2
+Nx_pasos = int(Lx / h+1)
+Ny_pasos = int(Ly / h+1)
 
 V = np.zeros((Nx_pasos, Ny_pasos))
 V_next = np.zeros((Nx_pasos, Ny_pasos))
 
-#Crear letra M
+# Crear letra M
 
-LetraM=[]
-for X in range(13,18):
-    for Y in range(21,56):
-        LetraM.append((X,Y))
-for X in range(18,23):
-    for Y in range(41,51):
-        LetraM.append((X,Y))        
-for X in range(23,28):
-    for Y in range(36,46):
-        LetraM.append((X,Y))
-for X in range(28,33):
-    for Y in range(41,51):
-        LetraM.append((X,Y))
-for X in range(33,38):
-    for Y in range(21,56):
-        LetraM.append((X,Y))
+LetraM = []
+for X in range(13, 18):
+    for Y in range(21, 56):
+        LetraM.append((X, Y))
+for X in range(18, 23):
+    for Y in range(41, 51):
+        LetraM.append((X, Y))
+for X in range(23, 28):
+    for Y in range(36, 46):
+        LetraM.append((X, Y))
+for X in range(28, 33):
+    for Y in range(41, 51):
+        LetraM.append((X, Y))
+for X in range(33, 38):
+    for Y in range(21, 56):
+        LetraM.append((X, Y))
 
-#Densidad de carga=
-#RHO=1./20./(h**2)
+# Densidad de carga=
+# RHO=1./20./(h**2)
 
-#Crear linea
+# Crear linea
 
-Abajo_Linea=[]
-for X in range(8,43):
-    Abajo_Linea.append((X,13))
-    
-Arriba_Linea=[]
-for X in range(8,43):
-    Arriba_Linea.append((X,14))
+Abajo_Linea = []
+for X in range(8, 43):
+    Abajo_Linea.append((X, 13))
+
+Arriba_Linea = []
+for X in range(8, 43):
+    Arriba_Linea.append((X, 14))
 
 # iteracion
-una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h, LetraM,Abajo_Linea,Arriba_Linea,w=1.)
+una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h,
+              LetraM, Abajo_Linea, Arriba_Linea, w=1.)
 counter = 1
-while counter < 1200 and no_ha_convergido(V, V_next, tolerancia=1e-5):
+while counter < 2000 and no_ha_convergido(V, V_next, tolerancia=1e-5):
     V = V_next.copy()
-    una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h, LetraM,Abajo_Linea,Arriba_Linea, w=1.)
+    una_iteracion(V, V_next, Nx_pasos, Ny_pasos, h,
+                  LetraM, Abajo_Linea, Arriba_Linea, w=1.)
     counter += 1
-V_next= V_next.transpose()
+V_next = V_next.transpose()
 print("counter = {}".format(counter))
 
 fig = plt.figure(1)
