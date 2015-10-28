b89193a67b5e95b02bf8cd6c7cbd421d555cc319
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-10-27 21:10:45 -0300

    Se define la forma de la letra usando funciones trazo y crear letra.

--- Poisson.py
+++ Poisson.py
@@ -12,17 +12,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import pdb
 
+L_x = 10
+L_y = 15
+h = 0.2
+
 def crear_caja(x, y, h):
     '''Recibe las dimensiones de la caja  y el tamaño del reticulado
-     y la inicia en puros ceros '''
+     y la inicia en ceros '''
     ancho_efectivo = x / h + 1
-    alto_efectivo = y / h + 1
-    caja = np.zeros((ancho_efectivo, alto_efectivo))
+    largo_efectivo = y / h + 1
+    caja = np.zeros((ancho_efectivo, largo_efectivo))
 
     return caja
 
 def poner_condiciones_borde(caja):
-    '''conecta a tierra el perímetro de la caja, la segunda iteración
+    '''Conecta a tierra el perímetro de la caja, la segunda iteración
     (bordes laterales) se itera desde borde+1, hasta borde para evitar pasar
     2 veces por las esquinas'''
     borde_inferior = np.array([np.array([-5, -7.5]), np.array([5, -7.5])])
@@ -39,16 +43,95 @@ def poner_condiciones_borde(caja):
         caja[borde_superior[1][0], i] = 0
     return caja
     
+def transformar(coordenada):
+    ''' Recibe coordenadas considerando 0,0 como el centro y
+    las dimensiones originales en centímetros, a las unidades de la grilla
+     (Notar que se utiliza Ancho+1 porque el arreglo se define desde 0)'''
 
-
-def dentro_letra_B(x, y, h):
+    x, y = coordenada[0], coordenada[1]
+    x_tran = int((x + (L_x)/2)/h)
+    y_tran = int((y + (L_y)/2)/h)
+    return x_tran, y_tran
     
+def es_horizontal(ini, fin):
+        '''Retorna true si el trazo es horizontal, false si es vertical'''
+        return (ini[1] == fin[1])
 
-def rho(x, y, h):
-    if dentro_letra_B(x, y, h):
-        return 1 / 20
+
+def trazo(ini, fin, ancho):
+    '''Recibe la coordenada de inicio del trazo, el final y el ancho del
+    trazo, devuelve un arreglo con las coordenadas de los puntos que lo
+    conformman. Sólo funciona para trazos rectos.
+    (recibe los puntos más izquierdos, o más  abajo del trazo)'''
+    ancho = int(ancho)+1
+    actual = ini
+    if es_horizontal(ini, fin):
+        paso = np.array([1, 0])
+        ancho_1 = np.array([0, 1])
+        rango = int(np.fabs(fin[0]-ini[0]))+1
     else:
-        return 0
+        paso = np.array([0, 1])
+        ancho_1 = np.array([1, 0])
+        rango = int(np.fabs(fin[1]-ini[1]))
+
+    trazo = np.zeros([(rango)*(ancho), 2])
+    for a in range(ancho):
+        for i in range(rango):
+            trazo[i+(rango)*a] = np.array([actual])
+            actual += paso
+        actual = ini
+        actual += (a+1)*ancho_1
+    
+    return trazo
+
+
+def dentro_letra_B(x, y):
+    '''Devuelve True si la coordenada está dentro del bloque que contiene
+    la letra'''
+    if (2.5 / h <= x and x <= 7.5 / h):
+        if(4 / h <= y and y <= 11 / h):
+            return True
+    return False
+    
+def armar_letra_B():
+    '''Devuelve el arreglo de coordenaas que conforman la letra B, las coordenadas
+     ini y fin se dan considerando 0,0 en el centro y
+     las dimensiones en centímetros'''
+    ancho = 1
+    ini = np.array([-2.5, -3.5])
+    fin = np.array([-2.5, 3.5])
+    ini2 = np.array([-1.5, 2.5])
+    fin2 = np.array([1.5, 2.5])
+    ini3 = np.array([-1.5, -0.5])
+    fin3 = np.array([1.5, -0.5])
+    ini4 = np.array([-1.5, -3.5])
+    fin4 = np.array([1.5, -3.5])
+    ini5 = np.array([1.5, 0.5])
+    fin5 = np.array([1.5, 2.5])
+    ini6 = np.array([1.5, -2.5])
+    fin6 = np.array([1.5, -0.5])
+
+    ancho_transf = ancho / h
+    ini, fin = transformar(ini), transformar(fin)
+    ini2, fin2 = transformar(ini2), transformar(fin2)
+    ini3, fin3 = transformar(ini3), transformar(fin3)
+    ini4, fin4 = transformar(ini4), transformar(fin4)
+    ini5, fin5 = transformar(ini5), transformar(fin5)
+    ini6, fin6 = transformar(ini6), transformar(fin6)
+
+    trazo_1 = trazo(ini, fin, ancho_transf)
+    trazo_2 = trazo(ini2, fin2, ancho_transf)
+    trazo_3 = trazo(ini3, fin3, ancho_transf)
+    trazo_4 = trazo(ini4, fin4, ancho_transf)
+    trazo_5 = trazo(ini5, fin5, ancho_transf)
+    trazo_6 = trazo(ini6, fin6, ancho_transf)
+    letra = np.append(trazo_1, trazo_2, axis=0)
+    letra = np.append(letra, trazo_3, axis=0)
+    letra = np.append(letra, trazo_4, axis=0)
+    letra = np.append(letra, trazo_5, axis=0)
+    letra = np.append(letra, trazo_6, axis=0)
+    return letra
+
 
 def Iteracion(V, V_next, N_pasos_x, N_pasos_y, h, w):
     for i in range(1, N_pasos_x-1):
@@ -59,6 +142,7 @@ def Iteracion(V, V_next, N_pasos_x, N_pasos_y, h, w):
                                     h**2 * rho(i, j, h)))
 
 def no_converge(V, V_next, tolerancia=1e-7):
+    ''' Devuelve True si es que la iteracion converge'''
     not_zero = (V_next != 0)
     diff_relativa = (V - V_next)[not_zero] / V_next[not_zero]
     max_diff = np.max(np.fabs(diff_relativa))
@@ -71,9 +155,6 @@ def no_converge(V, V_next, tolerancia=1e-7):
 
 #Inicializacion
 
-L_x = 10
-L_y = 15
-h = 0.2
 N_pasos_x = (L_x / h) + 1
 N_pasos_y = (L_y / h) + 1
 
