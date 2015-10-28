1e8ed14ca06de49c8d335725c5d16312f6e19d10
Nelsonism <nelson_bkn_1993@hotmail.com>
2015-10-28 04:00:39 -0300

    actualizado el metodo de sobrerelajacion

---
+++ eliminar.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def a(b):
+    y=b*0.2-7.5
+    return y
+    
--- tarea5.py
+++ tarea5.py
@@ -19,40 +19,42 @@ def LetraN(a, b):
     else:
         return 0
 
-def SobreRelax(V, w=1.4):
-    for i in range(1, 50):
-        for j in range(1, 10):
+def SobreRelax(V, w=1.3):
+    for i in range(1, 10):
+        for j in range(1, 75):
             V[i][j] = ((1 - w) * V[i][j] +
                             w / 4 * (V[i+1][j] + V[i-1][j] +
                                      V[i][j+1] + V[i][j-1] +
                                      h**2 * LetraN(i,j)))
-        for j in range(11, 75):
-            V[i][j] = ((1 - w) * V[i][j] +
-                            w / 4 * (V[i+1][j] + V[i-1][j] +
-                                     V[i][j+1] + V[i][j-1] +
-                                     h**2 * LetraN(i,j)))
-    for j in range(9, 12):
-        for i in range(1, 10):
-            V[i][j] = ((1 - w) * V[i][j] +
-                            w / 4 * (V[i+1][j] + V[i-1][j] +
-                                     V[i][j+1] + V[i][j-1] +
-                                     h**2 * LetraN(i,j)))
-        for i in range(41, 50):
+    for i in range(41, 50):
+        for j in range(1, 75):
             V[i][j] = ((1 - w) * V[i][j] +
                             w / 4 * (V[i+1][j] + V[i-1][j] +
                                      V[i][j+1] + V[i][j-1] +
                                      h**2 * LetraN(i,j)))
     for i in range(10, 41):
+        for j in range(1, 9):
+            V[i][j] = ((1 - w) * V[i][j] +
+                            w / 4 * (V[i+1][j] + V[i-1][j] +
+                                     V[i][j+1] + V[i][j-1] +
+                                     h**2 * LetraN(i,j)))
         for j in range(9, 10):
             V[i][j] = ((1 - w) * V[i][j] +
                             w / 3 * (V[i+1][j] + V[i-1][j] +
-                                     V[i][j-1] + h**2* LetraN(i,j) +
+                                     V[i][j-1] + h**2* LetraN(i,j)
                                      -h))
         for j in range(10, 11):
+            V[i][j] = V[i][j+1] - h
+        for j in range(11, 12):
             V[i][j] = ((1 - w) * V[i][j] +
                             w / 3 * (V[i+1][j] + V[i-1][j] +
-                                     V[i][j-1] + h**2 * LetraN(i,j) +
-                                     h))
+                                     V[i][j-1] + h**2* LetraN(i,j)
+                                     +h))
+        for j in range(12, 75):
+            V[i][j] = ((1 - w) * V[i][j] +
+                            w / 4 * (V[i+1][j] + V[i-1][j] +
+                                     V[i][j+1] + V[i][j-1] +
+                                     h**2 * LetraN(i,j)))
 
 Npuntos = []
 for a in range(51):
@@ -62,13 +64,18 @@ for a in range(51):
         
 Npuntos=np.arcsinh(np.transpose(Npuntos))
 plt.imshow(Npuntos, origin='bottom', interpolation='nearest', extent=[-5, 5, -7.5, 7.5])
+plt.colorbar()
 plt.show()
 
 V = np.zeros((51, 76))
-counter = 0
-while counter < 50:
-    SobreRelax(V)
-    counter += 1
+GuardaV = V
+SobreRelax(V) 
+N = 0
+while N < 400:
+    GuardaV = V
+    SobreRelax(V)   
+    N += 1
+print (N)
 
 V=np.arcsinh(np.transpose(V))
 plt.imshow(V, origin='bottom', interpolation='nearest', extent=[-5, 5, -7.5, 7.5])
