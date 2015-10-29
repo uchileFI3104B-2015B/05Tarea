fec4216a22328582331fb66a4b9d95d3a88de593
luzaguero <luz.francisca.ac@gmail.com>
2015-10-28 22:13:03 -0300

    Definir Una iteración

--- tarea5.py
+++ tarea5.py
@@ -23,7 +23,34 @@ def densidad_letra(i, j, h):
 
 
 def una_iteracion(V, Vnext, Nx_pasos, Ny_pasos, h, w=1.):
+    for i in range(1, int(Nx_pasos) - 1):
+        for j in range(1, 13):  #Bajo linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + w/4*(V[i + 1, j] +
+                            Vnext[i - 1, j] + V[i, j + 1] + Vnext[i, j - 1] +
+                                 h**2*densidad_letra(i, j, h)))
+        for j in range(15, int(Ny_pasos) - 1):  #Sobre linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + w/4*(V[i + 1, j] +
+                            Vnext[i - 1, j] + V[i, j + 1] + Vnext[i, j - 1] +
+                                 h**2*densidad_letra(i, j, h)))
+    for j in range(13, 15):
+        for i in range(1, 12):  #Izq. linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + w/4*(V[i + 1, j] +
+                            Vnext[i - 1, j] + V[i, j + 1] + Vnext[i, j - 1] +
+                                     h**2*densidad_letra(i, j, h)))
+        for i in range(42, int(Nx_pasos) - 1):  #Der. linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + (w/4)*(V[i + 1, j] +
+                            Vnext[i - 1, j] + V[i, j + 1] + Vnext[i, j - 1] +
+                                     h**2*densidad_letra(i, j, h)))
 
+    for i in range(12, 42):  #condicion derivada
+        for j in range(13, 14):  #puntos debajo linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + w/3*(V[i + 1, j] +
+                            Vnext[i - 1, j] + Vnext[i, j - 1] +
+                                     h**2*densidad_letra(i, j, h) + h*(-1.)))
+        for j in range(14, 15):  #puntos sobre linea
+            Vnext[i, j] = ((1 - w)*V[i, j] + w/3*(V[i + 1, j] +
+                            Vnext[i - 1, j] + Vnext[i, j - 1] +
+                                     h**2*densidad_letra(i, j, h) + h*1.))
 
 
 def no_ha_convergido(V, Vnext, tolerancia=1e-3):
